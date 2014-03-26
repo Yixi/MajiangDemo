5b44c6da7529586b35fe7f6499183b0789e21b81
Yixi <lyh1112@gmail.com>
2014-03-26 17:28:04 +0800

    finish main logic

--- src/draw.js
+++ src/draw.js
@@ -27,6 +27,8 @@ var Card = (function(){
         this.y = 0;
         this.width = 40;
         this.height = 60;
+        this.isSelect = false;
+        this.location = {x:null,y:null};
     }
 
     _Card.prototype.includePoint = function(x,y){
@@ -39,6 +41,61 @@ var Card = (function(){
     return _Card;
 })();
 
+function BaseLayer(){
+    this.x = 0;
+    this.y = 0;
+    this.id = null;
+    this.type = null;
+}
+
+function TextLayer(ctx,content,fontFamily,fontSize,fontColor){
+    this.type = 'text';
+    this.content = content;
+    this.fontFamily = fontFamily;
+    this.fontSize = fontSize;
+    this.fontColor = fontColor;
+    this.Ctx = ctx;
+
+    this.draw = function(){
+        this.clear();
+        this.Ctx.save();
+        this.Ctx.font = this.fontSize + 'px ' + this.fontFamily;
+        this.Ctx.textAlign = 'left';
+        this.Ctx.fillStyle = this.fontColor;
+        this.Ctx.fillText(this.content,this.x,this.y);
+        this.Ctx.restore();
+    };
+
+    this.clear = function(){
+
+    }
+}
+
+TextLayer.prototype = new BaseLayer();
+
+
+function Player(ctx,name){
+    this.scoreBoard = new TextLayer(ctx,'Score 0','Arial','20','green');
+    this._x = 0;
+    this._y = 0;
+    this.scoreBoard.x = this._x;
+    this.scoreBoard.y = this._y;
+    this.__defineSetter__('x',function(value){
+        this._x = value;
+        this.scoreBoard.x = value;
+    });
+    this.__defineSetter__('y',function(value){
+        this._y = value;
+        this.scoreBoard.y = value;
+    });
+    this.name = this.scoreBoard.id = name;
+    this.score = 0;
+
+    this.addScore = function(score){
+        this.score += score;
+        this.scoreBoard.content = 'Score ' + this.score;
+    }
+}
 
 
 
@@ -49,7 +106,11 @@ function MJ(element){
     Util.setAttr(this.Dom,'height',700);
     this.layers = [];
     this.cards = [];
+    this.currentPlayer = null;
+    this.lastSelect = null;
+    this.CurrentselectCard = null;
     this.initCards();
+    this.initScoreLayer();
     this.bindEvent();
 
 
@@ -81,26 +142,81 @@ MJ.prototype = {
                 z.cards[i][m] = _tmpCards[i*12 + m];
                 z.cards[i][m].x = m * ( z.cards[i][m].width + 5) + baseX;
                 z.cards[i][m].y = i * ( z.cards[i][m].height + 5) + baseY;
+                z.cards[i][m].location = {
+                    x:m,y:i
+                };
             }
         }
+    },
+
+    initScoreLayer:function(){
+        this.playerA = new Player(this.Ctx,'playerA');
+        this.playerA.x = 30; this.playerA.y = 30;
+        this.layers.push(this.playerA.scoreBoard);
+        this.currentPlayer = this.playerA;
+    },
+
+    cardCanSelect:function(card){
+        var z = this;
+        var x = card.location.x;
+        var y = card.location.y;
+        if(y < 1 || y>=8) {
+            return true
+        }
+        return !z.cards[y-1][x] || !z.cards[y+1][x]
     },
 
     /*bind Event*/
 
     bindEvent:function(){
         this.Dom.addEventListener('mousemove',this.mouseMove.bind(this),false);
+        this.Dom.addEventListener('mousedown',this.mouseDown.bind(this),false);
+    },
+
+    mouseDown:function(e){
+        if(this.CurrentselectCard){
+            if(!this.lastSelect){
+                if(this.cardCanSelect(this.CurrentselectCard)){
+                    this.lastSelect = this.CurrentselectCard;
+                    this.CurrentselectCard.isSelect = true;
+                }
+            }else{
+                if(this.CurrentselectCard == this.lastSelect){
+                    this.CurrentselectCard.isSelect = false;
+                    this.lastSelect = null;
+                }else{
+                    if(this.cardCanSelect(this.CurrentselectCard)){
+                        if(this.CurrentselectCard.name == this.lastSelect.name) {
+                            console.log('get point ' + this.lastSelect.value);
+                            this.currentPlayer.addScore(this.lastSelect.value);
+                            this.cleanCard(this.CurrentselectCard);
+                            this.cleanCard(this.lastSelect);
+                            this.lastSelect = null;
+                            this.CurrentselectCard = null;
+                        }else{
+                            this.lastSelect.isSelect = false;
+                            this.lastSelect = this.CurrentselectCard;
+                            this.lastSelect.isSelect = true;
+                        }
+                    }
+                }
+            }
+            this.renderView();
+        }
     },
 
     mouseMove:function(e){
-//        console.log(e.offsetX, e.offsetY);
         var z = this;
         var intersected = false;
+        z.CurrentselectCard = null;
         for(var i=0;i < z.cards.length;i++ ){
             for(var m=0;m< z.cards[i].length;m++){
                 var card = z.cards[i][m];
-                if(card.includePoint(e.offsetX, e.offsetY)){
+                if(!card) continue;
+                if(card.includePoint(e.offsetX, e.offsetY) && z.cardCanSelect(card)){
                     intersected = true;
-                    z.drawMarker(z.cards[i][m]);
+                    z.drawMarker(z.cards[i][m],'#000');
+                    z.CurrentselectCard = z.cards[i][m];
                 }
             }
         }
@@ -113,10 +229,14 @@ MJ.prototype = {
 
     /*draw*/
 
-    drawMarker:function(card){
+    cleanCard:function(card){
+        this.cards[card.location.y][card.location.x] = null;
+    },
+
+    drawMarker:function(card,color){
         this.renderView();
         this.Ctx.save();
-        this.Ctx.fillStyle = "#000";
+        this.Ctx.strokeStyle = color;
         this.Ctx.strokeRect(card.x,card.y,card.width,card.height);
 
         this.Ctx.restore();
@@ -126,10 +246,16 @@ MJ.prototype = {
     renderView:function(){
 
         this.Ctx.clearRect(0,0,this.Dom.width,this.Dom.height);
-
+        this.drawLayers();
         this.drawCards();
     },
-
+    drawLayers:function(){
+        var z = this;
+        for(var i=0;i<this.layers.length;i++){
+            var layer = z.layers[i];
+            layer.draw();
+        }
+    },
     drawCards:function(){
         var z = this,
             i = 0 ,
@@ -139,13 +265,24 @@ MJ.prototype = {
                 z.Ctx.save();
 
                 var card = z.cards[i][m];
+                if(!card) continue;
+
                 z.Ctx.fillStyle = '#c1c1c1';
                 z.Ctx.fillRect(card.x, card.y, card.width, card.height);
+
+                if(card.isSelect) {
+                    z.Ctx.strokeStyle = 'blue';
+                    z.Ctx.strokeRect(card.x,card.y,card.width,card.height);
+                    z.Ctx.fillStyle = 'rgba(0,0,255,.3)';
+                    z.Ctx.fillRect(card.x, card.y, card.width, card.height);
+                }
 
                 z.Ctx.font = "Bold 20px Arial";
                 z.Ctx.textAlign = 'center';
                 z.Ctx.fillStyle = '#000';
                 z.Ctx.fillText(card.name,card.x + 20,card.y + 40);
+
+
 
                 z.Ctx.restore();
             }
